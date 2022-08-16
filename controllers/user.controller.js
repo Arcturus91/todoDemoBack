ac260fd79e0b7f943e3d7bbe84faf0b886fb62a5
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-08-15 20:20:28 -0500

    CRUD completo

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,34 +1,96 @@
-const User = require ("../models/User.model")
+const User = require("../models/User.model");
 const mongoose = require("mongoose");
 const { clearRes, createJWT } = require("../utils/utils");
 
-exports.getLoggedUser = (req,res,next)=>{
+exports.getLoggedUser = (req, res, next) => {
+  res.status(200).json({ user: req.user });
+};
 
-res.status(200).json({user:req.user})    
+exports.editProfile = (req, res, next) => {
+  //destructuramos el rol para qe no lo peudan cambiar;
+  const { role, password, ...restUser } = req.body;
+  const { _id } = req.user;
+  User.findByIdAndUpdate(_id, { ...restUser }, { new: true })
+    .then((user) => {
+      const newUser = clearRes(user.toObject());
+      res.status(200).json({ user: newUser });
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        return res.status(400).json({ errorMessage: error.message });
+      }
+      if (error.code === 11000) {
+        return res.status(400).json({
+          errorMessage: "El correo electronico ya esta en uso.",
+        });
+      }
+      return res.status(500).json({ errorMessage: error.message });
+    });
+};
+
+exports.getUserById = (req, res, next) => {
+  const { id } = req.params;
+
+  User.findById(id)
+    .then((user) => {
+      const newUser = clearRes(user.toObject());
+      res.status(200).json({ user:newUser });
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        return res.status(400).json({ errorMessage: error.message });
+      }
+      if (error.code === 11000) {
+        return res.status(400).json({
+          errorMessage: "El correo electronico ya esta en uso.",
+        });
+      }
+      return res.status(500).json({ errorMessage: error.message });
+    });
+};
+
+
+//esta es para el admin:
+
+exports.onlyAdminRead = (req,res,next) =>{
+
+  User.find( { role: { $ne:"Admin"}}, {password:0,__v:0,createdAt:0,updatedAt:0} ) // estos operadores se ven muy pro.
+  .then(users=>{
+      res.status(200).json({ users })
+  })
+  .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        return res.status(400).json({ errorMessage: error.message });
+      }
+      if (error.code === 11000) {
+        return res.status(400).json({
+          errorMessage: "Hubo un error",
+        });
+      }
+      return res.status(500).json({ errorMessage: error.message });
+    });
 }
 
-exports.editProfile = (req,res,next)=>{
-    //destructuramos el rol para qe no lo peudan cambiar;
-const { role, password, ...restUser} = req.body;
-const {_id} = req.user
-User.findByIdAndUpdate(_id,{...restUser},{new:true})
-.then(user=>{
-    const newUser = clearRes(user.toObject());
-res.status(200).json({user:newUser})
-})
-.catch(
+//borrar la cuenta del usuario logeado
 
-    (error) => {
-        if (error instanceof mongoose.Error.ValidationError) {
-          return res.status(400).json({ errorMessage: error.message });
-        }
-        if (error.code === 11000) {
-          return res.status(400).json({
-            errorMessage: "El correo electronico ya esta en uso.",
-          });
-        }
-        return res.status(500).json({ errorMessage: error.message });
-      }
-)
+exports.deleteAccount = (req,res,next) =>{
+const {_id} = req.user;
+User.findByIdAndRemove(_id)
+.then(()=>{
+  res.clearCookie('headload')
+  res.clearCookie('signature')
+  res.status(200).json({successMessage:"Usuario borrado"})
+})
+.catch((error) => {
+  if (error instanceof mongoose.Error.ValidationError) {
+    return res.status(400).json({ errorMessage: error.message });
+  }
+  if (error.code === 11000) {
+    return res.status(400).json({
+      errorMessage: "Hubo un error",
+    });
+  }
+  return res.status(500).json({ errorMessage: error.message });
+})
 
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,22 @@
 const router = require("express").Router();
 
-const {getLoggedUser,editProfile} = require ("../controllers/user.controller");
+const {getLoggedUser,editProfile,getUserById,deleteAccount,onlyAdminRead} = require ("../controllers/user.controller");
 
-const {verifyToken} = require ("../middleware")
+const {verifyToken,checkRole} = require ("../middleware")
 
 router.get("/my-profile", verifyToken, getLoggedUser)
 
 router.patch("/edit-profile", verifyToken, editProfile)
 
-//router.delete("/delete-user")
+router.delete("/delete-user",verifyToken,deleteAccount)
 
 //read - other user
 
-router.get("/:id/profile")
+router.get("/:id/profile",verifyToken, getUserById)
+
+//read all user (para admin staff)
+
+router.get("/admin/users",verifyToken,  checkRole(["Admin"]),onlyAdminRead)
 
 
 
